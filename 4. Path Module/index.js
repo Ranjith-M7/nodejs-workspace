75df5ada8e78f99678d1a60221445c438bbdae99
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-11-13 08:08:44 +0530

    updated

--- 4. Path Module/index.js
+++ 4. Path Module/index.js
@@ -1,5 +1,20 @@
 import path from "path";
 
-console.log(path.basename("c://nodejs//app.html"));
+// console.log(path.basename("c://nodejs//app.html"));
+// console.log(path.basename("c://nodejs//app.js", ".js"));
+// console.log(path.dirname("c:\\nodejs\\courses\\app.js"));
+// console.log(path.extname("c:\\nodejs\\courses\\app.js"));
+// console.log(path.join("c:", "huxn", "courses", "redux-toolkit"));
 
-console.log(path.basename("c://nodejs//app.js", ".js"));
+// Go up a level
+// console.log(path.join("c:", "huxn", "courses", "redux-toolkit", ".."));
+// console.log(path.join("c:", "huxn", "courses", "redux-toolkit", "..", ".."));
+
+// console.log(path.normalize("courses\\redux\\store\\..//features"));
+
+console.log(path.parse("c:\\nodejs\\app.js"));
+console.log(path.parse("c:\\nodejs\\app.js").base);
+console.log(path.parse("c:\\nodejs\\app.js").name);
+console.log(path.parse("c:\\nodejs\\app.js").ext);
+console.log(path.parse("c:\\nodejs\\app.js").dir);
+console.log(path.parse("c:\\nodejs\\app.js").root);
