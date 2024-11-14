acedbd9089df59b42ef00258c2ac260ed4cf6faf
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-11-14 23:48:08 +0530

    updated

---
+++ 5. FS Module/1. Promise api/index.js
@@ -0,0 +1,30 @@
+import * as fs from "fs/promises";
+
+// Creating directory or folder
+// try {
+//   await fs.mkdir("D:\\Huxn\\webdev\\react\\node", { recursive: true });
+//   console.log("Folder created");
+// } catch (error) {
+//   console.log(error);
+// }
+
+// Read the directory / folder
+
+// try {
+//   const files = await fs.readdir("D:\\xuxn\\node");
+//   for (const file of files) {
+//     console.log(file);
+//   }
+//   console.log("Folder read");
+// } catch (error) {
+//   console.log(error);
+// }
+
+// Remove the directory or folder
+
+// try {
+//   await fs.mkdir("D:\\xuxn\\node");
+//   console.log("Folder read");
+// } catch (error) {
+//   console.log(error);
+// }
