3f425f365f58209e1318fddc016486b265baf7ad
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-11-18 23:24:23 +0530

    updated http module

---
+++ 8. HTTP Module/index.js
@@ -0,0 +1,18 @@
+import http from "http";
+
+// Create our server
+const server = http.createServer((req, res) => {
+  //   console.log(req);
+  //   res.setHeader("Content-Type", "text/html");
+  //   res.statusCode = 202;
+  //   res.statusMessage = "Good";
+
+  res.writeHead(404, "Bad", { "Content-type": "text/html" });
+
+  res.write("<h1>Hello From Node.js Server</h1>");
+});
+
+// Listening on port 8080
+server.listen(8000, () => {
+  console.log("Server Up!");
+});
